8bbd88d53b3f5b44df59463aeaa85170064505be
Ant Flaherty <antrflaherty@gmail.com>
2026-09-21 09:40:06 -0700

    Apply theme to tabs

--- frontend/src/app/(tabs)/_layout.tsx
+++ frontend/src/app/(tabs)/_layout.tsx
@@ -1,32 +1,47 @@
 import { Tabs } from "expo-router";
 import { AntDesign } from "@react-native-vector-icons/ant-design";
+import { useTheme } from "@/app/theme";
 
 export default function TabLayout() {
+  const { theme } = useTheme();
+
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarStyle: {
+          backgroundColor: theme.background,
+          borderTopColor: theme.background,
+        },
+        tabBarActiveTintColor: theme.text,
+        tabBarInactiveTintColor: theme.text,
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({ size }) => <AntDesign name="home" size={size} />,
+          tabBarIcon: ({ size }) => (
+            <AntDesign name="home" color={theme.primary} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
         name="log-reading"
         options={{
           title: "Log Reading",
-          tabBarIcon: ({ size }) => <AntDesign name="book" size={size} />,
+          tabBarIcon: ({ size }) => (
+            <AntDesign name="book" color={theme.primary} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: "Settings",
-          tabBarIcon: ({ size }) => <AntDesign name="setting" size={size} />,
+          tabBarIcon: ({ size }) => (
+            <AntDesign name="setting" color={theme.primary} size={size} />
+          ),
         }}
       />
     </Tabs>
