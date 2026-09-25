6ffdb3524da37a2a53ca9780a36d08577303348a
Ant Flaherty <antrflaherty@gmail.com>
2026-09-25 10:26:00 -0700

    Configure header to go back

--- frontend/src/app/(authenticated)/_layout.tsx
+++ frontend/src/app/(authenticated)/_layout.tsx
@@ -1,13 +1,24 @@
 import { Stack } from "expo-router";
 import ShelfProvider from "@/context/shelf-provider";
+import { useTheme } from "@/theme/theme-provider";
 
 export default function AuthenticatedLayout() {
+  const { theme } = useTheme();
+
   return (
     <ShelfProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 
-        <Stack.Screen name="book" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="book"
+          options={{
+            headerTintColor: theme.background,
+            headerBackButtonDisplayMode: "generic",
+            headerTransparent: true,
+            title: "",
+          }}
+        />
       </Stack>
     </ShelfProvider>
   );
